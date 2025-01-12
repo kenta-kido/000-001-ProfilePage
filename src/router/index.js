86f3da18e2b97deeebaebec88f86833c1bba1ca1
Kenta Kido <kido.0117.1895@gmail.com>
2025-01-12 19:48:57 +0100

    add basic components

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '@/views/HomePage.vue';
 import PortfolioPage from '@/views/PortfolioPage.vue';
+import AboutPage from '@/views/AboutPage.vue';
+import ContactPage from '@/views/ContactPage.vue';
+
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/portfolio', component: PortfolioPage }
+    { path: '/', name: 'Home', component: HomePage },
+    { path: '/portfolio', name: 'Portfolio', component: PortfolioPage },
+    { path: '/about', name: 'About', component: AboutPage },
+    { path: '/contact', name: 'Contact', component: ContactPage },
 ];
 
 const router = createRouter({
